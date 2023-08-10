00c03360401416cf6ab26a60d6a7687c89c98cac
Jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-08-10 13:46:31 +0100

    added HelloWorld

---
+++ exercises/getting-started/src/com/codingblackfemales/academy/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.codingblackfemales.academy;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }//end of main
+
+    }
+    
+
