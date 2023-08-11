26d05f72e006148c2ee51a17cd71a620350f5857
Jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-08-11 22:52:05 +0100

    added Arithmetic file to practise using operators

---
+++ exercises/java-primitives/src/main/java/com/cbfacademy/Arithmetic.java
@@ -0,0 +1,16 @@
+package com.cbfacademy;
+
+public class Arithmetic {
+    float AddFloat(float operand1, float operand2) {
+        return operand1 + operand2;
+    }
+    float SubtractFloat(float operand1, float operand2) {
+        return operand1 - operand2;
+    }
+    float MultiplyFloat(float operand1, float operand2) {
+        return operand1 * operand2;
+    }
+    float DivideFloat(float operand1, float operand2) {
+        return operand1 / operand2;
+    }
+}
